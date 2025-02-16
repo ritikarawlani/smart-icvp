4d3b6184a8619a686ba1b8e47fafee39a091b609
Carl Leitner <litlfred@gmail.com>
2025-02-16 05:54:45 -0500

    in output sub-dirs copy xml and ttl too

--- makeResourceDirsInOutput.sh
+++ makeResourceDirsInOutput.sh
@@ -5,13 +5,16 @@ for resource in output/*.json ; do
 
     resourceType=$(jq -re '.resourceType' $resource)
     resourceID=$(jq -re '.id' $resource)
-    if [[ (-n "$resourceType" ) && ( "$resourceType" != "null") ]]; then
-	echo $resource " -> " $resourceType
-	echo "Making output/$resourceType"
-	mkdir -p output/$resourceType
-	echo "Copying $resource to output/$resourceType/$resourceID.json"
-	cp $resource output/$resourceType/$resourceID.json
+    if [[ (-n "$resourceType" ) && ( "$resourceType" != "null") \
+          && (-n "$resourceID" ) && ( "$resourceID" != "null") ]]; then
 
+	mkdir -p output/$resourceType
+
+	echo "Copying output/$resourceType-$resourceID.<blah> to output/$resourceType/$resourceID.<blah>"
+	cp output/$resourceType-$resourceID.json output/$resourceType/$resourceID.json
+	cp output/$resourceType-$resourceID.xml output/$resourceType/$resourceID.xml
+	cp output/$resourceType-$resourceID.ttl output/$resourceType/$resourceID.ttl
+	
 	
     fi
 done
