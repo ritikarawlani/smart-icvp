b01f4f6af8ee31cdd6aaef3d3a974aa8c01c2b45
Carl Leitner <litlfred@gmail.com>
2025-02-16 06:30:42 -0500

    prettyify the output/blah/reousrce.html pages

--- makeResourceDirsInOutput.sh
+++ makeResourceDirsInOutput.sh
@@ -4,6 +4,8 @@
 for resource in output/*.json ; do
 
     resourceType=$(jq -re '.resourceType' $resource)
+    resourceURL=$(jq -re '.url' $resource)
+    resourceVersion=$(jq -re '.version' $resource)
     resourceID=$(jq -re '.id' $resource)
     if [[ (-n "$resourceType" ) && ( "$resourceType" != "null") \
           && (-n "$resourceID" ) && ( "$resourceID" != "null") ]]; then
@@ -14,7 +16,51 @@ for resource in output/*.json ; do
 	cp output/$resourceType-$resourceID.json output/$resourceType/$resourceID.json
 	cp output/$resourceType-$resourceID.xml output/$resourceType/$resourceID.xml
 	cp output/$resourceType-$resourceID.ttl output/$resourceType/$resourceID.ttl
-	cp output/$resourceType-$resourceID.html output/$resourceType/$resourceID.html
+	echo "<!DOCTYPE html>
+<html xml:lang='en' xmlns='http://www.w3.org/1999/xhtml' lang='en'>
+<html>
+ <head>
+    <style class='anchorjs'></style><link href='../fhir.css' rel='stylesheet'>
+    <link href='../assets/css/fa-all.css' rel='stylesheet' type='text/css'>
+    <!-- Bootstrap core CSS -->
+    <link href='../assets/css/bootstrap-fhir.css' rel='stylesheet'>
+
+    <!-- Project extras -->
+    <link href='../assets/css/project.css' rel='stylesheet'>
+    <link href='../assets/css/pygments-manni.css' rel='stylesheet'>
+    <link href='../assets/css/jquery-ui.css' rel='stylesheet'>
+  	<link href='../assets/css/prism.css' rel='stylesheet'>
+    <!-- Placeholder for child template CSS declarations -->
+    <link href='../assets/css/who.css' rel='stylesheet'>
+    <link href='../assets/css/lforms.min.css' rel='stylesheet'>
+ </head>
+ <body><div class='row'><div class='container'>
+  <table class='colsi'>
+    <tbody>
+      <tr>
+       <td colspan='4'>
+         <i>Official URL</i>: <span class='copy-text'>$resourceURL<button title='' class='btn-copy' data-clipboard-text='$resourceURL' data-original-title='Click to copy URL'>
+      </button></span></td>
+      <td><i>Version</i>:
+      <span class='copy-text'>$resourceVersion<button title='' class='btn-copy' data-clipboard-text='$resourceURL|$resourceVersion' data-original-title='Click to copy versioned URL'>
+      </button></span></td>
+    </tr>
+    <tr>
+      <td colspan='4'>
+       <ul>
+        <li><a href='../$resourceType-$resourceID.html'>Narrative Content</a></li>
+        <li><a href='$resourceID.json'>JSON</a></li>
+        <li><a href='$resourceID.xml'>XML</a></li>
+        <li><a href='$resourceID.ttl'>TTL</a></li>
+       </ul>
+      </td>
+      <td><i>Computable Name</i>: <span style='font-family: monospace;'>$resourceID</span></td>
+     </tr>
+    </tbody>
+   </table>
+ </div></div></body>
+</html>
+" >  output/$resourceType/$resourceID.html
 	
 	
     fi
